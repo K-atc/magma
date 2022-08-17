787cdc7d21b204bf96d97e865487c8b76fda45e7
K_atc <shiftx1026@gmail.com>
2022-08-17 17:00:39 +0900

    Update make openssl traceable

--- targets/openssl/build.sh
+++ targets/openssl/build.sh
@@ -7,6 +7,7 @@ set -e
 # - env OUT: path to directory where artifacts are stored
 # - env CC, CXX, FLAGS, LIBS, etc...
 ##
+set -eux
 
 if [ ! -d "$TARGET/repo" ]; then
     echo "fetch.sh must be executed first."
@@ -24,14 +25,14 @@ fi
 # the config script supports env var LDLIBS instead of LIBS
 export LDLIBS="$LIBS"
 
-./config --debug enable-fuzz-libfuzzer enable-fuzz-afl disable-tests -DPEDANTIC \
+ls Makefile || ./config --debug enable-fuzz-libfuzzer enable-fuzz-afl disable-tests -DPEDANTIC \
     -DFUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION no-shared no-module \
     enable-tls1_3 enable-rc5 enable-md2 enable-ec_nistp_64_gcc_128 enable-ssl3 \
     enable-ssl3-method enable-nextprotoneg enable-weak-ssl-ciphers \
     $CFLAGS -fno-sanitize=alignment $CONFIGURE_FLAGS
 
-make -j$(nproc) clean
-make -j$(nproc) LDCMD="$CXX $CXXFLAGS"
+# make -j$(nproc) clean
+bear -- make -j$(nproc) LDCMD="$CXX $CXXFLAGS"
 
 fuzzers=$(find fuzz -executable -type f '!' -name \*.py '!' -name \*-test '!' -name \*.pl)
 for f in $fuzzers; do
